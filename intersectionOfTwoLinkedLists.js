9ac405c448d005aaeabcef6b623751aea579d6ea
Sean <shonum@gmail.com>
2016-11-11 17:53:19 -0800

    Complete logic for intersectionOfTwoLinkedLists

--- intersectionOfTwoLinkedLists.js
+++ intersectionOfTwoLinkedLists.js
@@ -14,6 +14,7 @@ B:     b1 → b2 → b3
 begin to intersect at node c1.
 
 
+
 Notes:
 
 If the two linked lists have no intersection at all, return null.
@@ -39,8 +40,45 @@ class ListNode {
   }
 }
 
+const checkLength = (node, counter=0) => {
+  counter++;
+  if (node.next !== null) {
+    let nextNode = node.next;
+    return checkLength(nextNode, counter);
+  }
+  return counter;
+};
+
 var getIntersectionNode = function(headA, headB) {
+  // initialize 2 counters. one for headA and another for headB.
+  headA.length = checkLength(headA);
+  headB.length = checkLength(headB);
+  let longer, shorter, offset, start;
+  if (headA.length >= headB.length) {
+    longer = headA;
+    shorter = headB;
+    offset = headA.length - headB.length;
+  } else {
+    longer = headB;
+    shorter = headA;
+    offset = headB.length - headA.length;
+  }
+  for (let i = 0; i < offset; i++) {
+    longer = longer.next;
+  }
+
+  for (let i = 0; i < shorter.length; i++) {
+    if (longer.val === shorter.val) {
+      return longer;
+    }
+    longer = longer.next;
+    shorter = shorter.next;
+  }
+  return null;
 
 };
 
 let newList = new ListNode(1);
+let newList2 = new ListNode(2);
+newList.next = new ListNode(5);
+console.log(newList.next);
