ccd2c9ac9448f86c52594f2a4f3e17ae02082081
Sean <shonum@gmail.com>
2016-12-01 11:36:27 -0800

    Complete day1

--- adventOfCode/day1.js
+++ adventOfCode/day1.js
@@ -20,9 +20,52 @@ For example:
 
 How many blocks away is Easter Bunny HQ?
 
-Input: [R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2]
+Input: R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2
 
 Output: Number of blocks
 
 */
+
+const numberOfBlocks = (str) => {
+  let array = str.split(', ');
+  let direction = "North";
+  let lat = 0;
+  let lng = 0;
+
+  const help = {
+    North: {
+      R: 'East',
+      L: 'West',
+      move: dist => lng -= dist
+    },
+    South: {
+      R: 'West',
+      L: 'East',
+      move: dist => lng += dist
+    },
+    East: {
+      R: 'South',
+      L: 'North',
+      move: dist => lat += dist
+    },
+    West: {
+      R: 'North',
+      L: 'South',
+      move: dist => lat -= dist
+    }
+  };
+
+  array.forEach(instr=>{
+    let turn = instr.slice(0, 1);
+    let dist = instr.slice(1) * 1;
+
+    direction = help[direction][turn];
+    help[direction].move(dist);
+  });
+
+  return Math.abs(lat) + Math.abs(lng);
+
+};
+
+console.log(numberOfBlocks('R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2'));
 
